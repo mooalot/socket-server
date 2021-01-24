05f5c472a0e8134a1bde03bc29f2177ed89cad25
Gabriel Saunders <igabe@icloud.com>
2021-01-24 15:14:10 -0700

    first

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+app.use(cors())
+let server = require('http').createServer(app);
+let io = require('socket.io')(server, {
+  cors: {
+    origin: true,
+    credentials: true,
+    methods: ["GET", "POST"],
+  }
+});
+
+io.on('connection', (socket) => {
+ 
+  socket.on('disconnect', function(){
+    io.emit('users-changed', {user: socket.username, event: 'left'});   
+  });
+ 
+  socket.on('set-name', (name) => {
+    socket.username = name;
+    io.emit('users-changed', {user: name, event: 'joined'});    
+  });
+  
+  socket.on('send-message', (message) => {
+    io.emit('message', {msg: message.text, user: socket.username, createdAt: new Date()});    
+  });
+});
+ 
+var port = process.env.PORT || 3001;
+ 
+server.listen(port, function(){
+   console.log('listening in http://localhost:' + port);
+});
